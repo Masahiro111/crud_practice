8e4e849213d7e69407181f191e83301d8e5e167e
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-08-09 19:12:16 +0900

    save 2

--- routes/back.php
+++ routes/back.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Back\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    echo 'back';
-});
+Route::resource('posts', PostController::class)->except('show');
--- routes/front.php
+++ routes/front.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    echo 'front';
-});
+Route::get('/', [PostController::class, 'index'])
+    ->name('home');
+
+Route::resource('/posts', PostController::class)
+    ->only('index', 'show');
